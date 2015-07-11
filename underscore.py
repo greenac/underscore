b76418f5d8a6507c23242d86ca049ada7bd7d9e4
Andre Green <acgreen1226@gmail.com>
2015-07-10 23:46:41 -0700

    rename directories under the root directory with same rules as files

--- underscore.py
+++ underscore.py
@@ -1,12 +1,13 @@
 import os
 import shutil
 
+
 class Underscore:
     def __init__(self, root_path, replacement_char, chars_to_replace):
         self.root_path = root_path
         self.replacement_char = replacement_char
-        self.pic_extensions = ({'png', 'jpg'})
         self.chars_to_replace = chars_to_replace
+        self.pic_extensions = ({'png', 'jpg'})
 
     def start(self):
         self.add_underscores(self.root_path)
@@ -18,7 +19,10 @@ class Underscore:
             for file in files:
                 file_path = os.path.join(path, file)
                 if os.path.isdir(file_path):
-                    self.add_underscores(file_path)
+                    new_file = self.new_file_name(file)
+                    new_file_path = os.path.join(path, new_file)
+                    shutil.move(file_path, new_file_path)
+                    self.add_underscores(new_file_path)
                 else:
                     file_parts = file.split('.')
                     if (len(file_parts)) > 1:
